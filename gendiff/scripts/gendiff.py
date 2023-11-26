c0734063efb270989376f2d427811fe03634c21a
ithemask <ai77xxxx@gmail.com>
2023-11-26 18:09:30 +0300

    Update gendiff.py script
    Add plain JSON file comparison functionality

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+from gendiff import generate_diff
 
 
 def main():
@@ -9,7 +10,8 @@ def main():
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     parser.add_argument("-f", "--format", help="set format of output")
-    parser.parse_args()
+    args = parser.parse_args()
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
