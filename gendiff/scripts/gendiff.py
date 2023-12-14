3b15716ddf9ff8fcb32a6bb97e6cd9bdebc86e83
ithemask <ai77xxxx@gmail.com>
2023-12-14 18:16:31 +0300

    Update gendiff.py script
    Set default output formatter; add alternative output formatter

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 import argparse
-from gendiff import generate_diff
+from gendiff.gendiff import generate_diff
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference."
+        description="Compares two configuration files and shows a difference.",
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
-    parser.add_argument("-f", "--format", help="set format of output")
+    parser.add_argument(
+        "-f",
+        "--format",
+        default="stylish",
+        choices=["stylish", "plain"],
+        help="set format of output (default: \"stylish\")",
+    )
     args = parser.parse_args()
-    print(generate_diff(args.first_file, args.second_file))
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == '__main__':
