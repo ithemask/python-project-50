483bebd04f7244f3c6e9483f1d3e84eb10330c18
ithemask <ai77xxxx@gmail.com>
2023-12-14 17:31:01 +0300

    Refactor gendiff.py module
    Modify generate_diff func; isolate diff calculation process; isolate formatting process

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,34 +1,16 @@
-from gendiff.input_parser import parse_input
+from gendiff.diff import get_diff
+from gendiff.formatters.stylish import stylish
+from gendiff.formatters.plain import plain
 
 
-DEFAULT_VALUE = "impossible_value"
-
-
-def format_(value):
-    return str(value).lower() if value in (True, False, None) else value
-
-
-def update_output(output, prefix, key, value):
-    output.append(f"  {prefix} {key}: {format_(value)}")
-
-
-def generate_diff(file_path1, file_path2):
-    output = []
-    config1 = parse_input(file_path1)
-    config2 = parse_input(file_path2)
-    keys = sorted(list(set(config1) | set(config2)))
-
-    for key in keys:
-        value1 = config1.get(key, DEFAULT_VALUE)
-        value2 = config2.get(key, DEFAULT_VALUE)
-        if value1 == value2:
-            update_output(output, " ", key, value1)
-        elif DEFAULT_VALUE not in (value1, value2):
-            update_output(output, "-", key, value1)
-            update_output(output, "+", key, value2)
-        elif value2 == DEFAULT_VALUE:
-            update_output(output, "-", key, value1)
-        else:
-            update_output(output, "+", key, value2)
-
-    return "{\n" + "\n".join(output) + "\n}"
+def generate_diff(file_path1, file_path2, output_format="stylish"):
+    if output_format == "stylish":
+        apply_format = stylish
+    elif output_format == "plain":
+        apply_format = plain
+    else:
+        raise ValueError(
+            f'Invalid output format "{output_format}". ' +
+            'Available formats are: "stylish", "plain".'
+        )
+    return apply_format(get_diff(file_path1, file_path2))
