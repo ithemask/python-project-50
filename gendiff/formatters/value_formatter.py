e9b10c8736d0de551c6190dffdd055a7fe7f876f
ithemask <ai77xxxx@gmail.com>
2023-12-18 17:09:04 +0300

    Update value_formatter.py module
    Implement format_more func

--- gendiff/formatters/value_formatter.py
+++ gendiff/formatters/value_formatter.py
@@ -4,3 +4,11 @@ def format_(value):
     if value is None:
         return "null"
     return str(value)
+
+
+def format_more(value):
+    if isinstance(value, dict):
+        return "[complex value]"
+    if isinstance(value, str):
+        return f"'{value}'"
+    return format_(value)
