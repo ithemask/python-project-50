2b7880b8f9a1447a9c038abe7fd1f17ba9d1592e
ithemask <ai77xxxx@gmail.com>
2023-12-18 17:08:02 +0300

    Update tests/test_generate_diff.py module

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -6,14 +6,17 @@ from gendiff.gendiff import generate_diff
 FILE_PATH1 = "tests/fixtures/nested1.yml"
 FILE_PATH2 = "tests/fixtures/nested2.json"
 OUTPUT_PATH1 = "tests/fixtures/nested_stylished1.txt"
+OUTPUT_PATH2 = "tests/fixtures/nested_plained1.txt"
 
 
 expected1 = parse_txt(OUTPUT_PATH1)
+expected2 = parse_txt(OUTPUT_PATH2)
 
 
 @pytest.mark.parametrize("test_inputs, expected", [
         ((FILE_PATH1, FILE_PATH2), expected1),
         ((FILE_PATH1, FILE_PATH2, "stylish"), expected1),
+        ((FILE_PATH1, FILE_PATH2, "plain"), expected2),
     ])
 def test_generate_diff(test_inputs, expected):
     assert generate_diff(*test_inputs) == expected
