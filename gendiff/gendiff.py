befab7862c4d506011cb6e22a2e6a6ee8d15287d
ithemask <ai77xxxx@gmail.com>
2023-12-18 18:56:29 +0300

    Update gendiff.py module
    Modify generate_diff func; add "json" output format

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,6 +1,7 @@
 from gendiff.diff import get_diff
 from gendiff.formatters.stylish import stylished
 from gendiff.formatters.plain import plained
+from gendiff.formatters.json_ import jsoned
 
 
 def generate_diff(file_path1, file_path2, output_format="stylish"):
@@ -8,9 +9,11 @@ def generate_diff(file_path1, file_path2, output_format="stylish"):
         apply_format = stylished
     elif output_format == "plain":
         apply_format = plained
+    elif output_format == "json":
+        apply_format = jsoned
     else:
         raise ValueError(
             f'Invalid output format "{output_format}". ' +
-            'Available formats are: "stylish", "plain".'
+            'Available formats are: "stylish", "plain", "json".'
         )
     return apply_format(get_diff(file_path1, file_path2))
