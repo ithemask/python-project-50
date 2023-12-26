3698b24f90dfaf4801935b3f4db6a6329e9c50e2
ithemask <ai77xxxx@gmail.com>
2023-12-26 23:40:47 +0300

    Update tests/fixture_parser.py module
    Remove parse_json func

--- tests/fixture_parser.py
+++ tests/fixture_parser.py
@@ -4,8 +4,3 @@ from json import load
 def parse_txt(file_path):
     with open(file_path) as file:
         return file.read().strip()
-
-
-def parse_json(file_path):
-    with open(file_path) as file:
-        return load(file)
