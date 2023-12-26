39f92c26794f016a8c4fc821eb66a13cf74d0233
ithemask <ai77xxxx@gmail.com>
2023-12-26 19:10:07 +0300

    Add arg_parser.py module
    Add parse_args func

---
+++ gendiff/arg_parser.py
@@ -0,0 +1,17 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference.",
+    )
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument(
+        "-f",
+        "--format",
+        default="stylish",
+        choices=["stylish", "plain", "json"],
+        help="set format of output (default: \"stylish\")",
+    )
+    return parser.parse_args()
