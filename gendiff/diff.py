437f502cc79e71bd04aa94b216f40b9407427ab2
ithemask <ai77xxxx@gmail.com>
2023-12-26 22:46:19 +0300

    Update diff.py module
    Modify get_diff func; implement get_action func; change diff view

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,21 +1,32 @@
 DEFAULT_VALUE = "nonexistent"
 
 
+def get_action(old_value, new_value):
+    if old_value == DEFAULT_VALUE:
+        return 'ADDED'
+    if new_value == DEFAULT_VALUE:
+        return 'REMOVED'
+    if old_value != new_value:
+        return 'CHANGED'
+    return 'UNCHANGED'
+
+
 def get_diff(dict1, dict2):
     diff = []
     keys = sorted(list(set(dict1) | set(dict2)))
 
     for key in keys:
-        value1 = dict1.get(key, DEFAULT_VALUE)
-        value2 = dict2.get(key, DEFAULT_VALUE)
+        old_value = dict1.get(key, DEFAULT_VALUE)
+        new_value = dict2.get(key, DEFAULT_VALUE)
 
-        if isinstance(value1, dict) and isinstance(value2, dict):
-            diff.append({"key": key, "nested": get_diff(value1, value2)})
+        if isinstance(old_value, dict) and isinstance(new_value, dict):
+            diff.append({"key": key, "nested": get_diff(old_value, new_value)})
         else:
             diff.append({
                 "key": key,
-                "old value": value1,
-                "new value": value2,
+                "action": get_action(old_value, new_value),
+                "old value": old_value,
+                "new value": new_value,
             })
 
     return diff
