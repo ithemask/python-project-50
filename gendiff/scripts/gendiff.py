3f728a27ca961e90be8a8aa85b25787d97ef82ec
ithemask <ai77xxxx@gmail.com>
2023-12-26 19:01:06 +0300

    Refactor gendiff.py script
    Modify main func; isolate command-line parsing process

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,22 +1,10 @@
 #!/usr/bin/env python3
-import argparse
+from gendiff.arg_parser import parse_args
 from gendiff.gendiff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference.",
-    )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
-    parser.add_argument(
-        "-f",
-        "--format",
-        default="stylish",
-        choices=["stylish", "plain", "json"],
-        help="set format of output (default: \"stylish\")",
-    )
-    args = parser.parse_args()
+    args = parse_args()
     print(generate_diff(args.first_file, args.second_file, args.format))
 
 
