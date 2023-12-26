6f629d318824fda4b11ddd2fd5c815db2dcd05c5
ithemask <ai77xxxx@gmail.com>
2023-12-26 19:41:19 +0300

    Refactor diff.py module; refactor gendiff.py module
    Move input parsing process from get_diff func to generate_diff func

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,30 +1,21 @@
-from gendiff.input_parser import parse_input
-
-
 DEFAULT_VALUE = "nonexistent"
 
 
-def get_diff(file_path1, file_path2):
-    config1 = parse_input(file_path1)
-    config2 = parse_input(file_path2)
+def get_diff(dict1, dict2):
+    diff = []
+    keys = sorted(list(set(dict1) | set(dict2)))
 
-    def walk(dict1, dict2):
-        diff = []
-        keys = sorted(list(set(dict1) | set(dict2)))
+    for key in keys:
+        value1 = dict1.get(key, DEFAULT_VALUE)
+        value2 = dict2.get(key, DEFAULT_VALUE)
 
-        for key in keys:
-            value1 = dict1.get(key, DEFAULT_VALUE)
-            value2 = dict2.get(key, DEFAULT_VALUE)
+        if isinstance(value1, dict) and isinstance(value2, dict):
+            diff.append({"key": key, "nested": get_diff(value1, value2)})
+        else:
+            diff.append({
+                "key": key,
+                "old value": value1,
+                "new value": value2,
+            })
 
-            if isinstance(value1, dict) and isinstance(value2, dict):
-                diff.append({"key": key, "nested": walk(value1, value2)})
-            else:
-                diff.append({
-                    "key": key,
-                    "old value": value1,
-                    "new value": value2,
-                })
-
-        return diff
-
-    return walk(config1, config2)
+    return diff
--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,3 +1,4 @@
+from gendiff.input_parser import parse_input
 from gendiff.diff import get_diff
 from gendiff.formatters.stylish import stylished
 from gendiff.formatters.plain import plained
@@ -5,6 +6,9 @@ from gendiff.formatters.json_ import jsoned
 
 
 def generate_diff(file_path1, file_path2, output_format="stylish"):
+    config1 = parse_input(file_path1)
+    config2 = parse_input(file_path2)
+
     if output_format == "stylish":
         apply_format = stylished
     elif output_format == "plain":
@@ -16,4 +20,5 @@ def generate_diff(file_path1, file_path2, output_format="stylish"):
             f'Invalid output format "{output_format}". '
             + 'Available formats are: "stylish", "plain", "json".'
         )
-    return apply_format(get_diff(file_path1, file_path2))
+
+    return apply_format(get_diff(config1, config2))
