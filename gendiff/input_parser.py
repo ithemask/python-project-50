bb63dcf5ef75d52b56633df5aaa84058698eb2ae
ithemask <ai77xxxx@gmail.com>
2024-02-14 18:54:11 +0300

    Refactor input_parser.py module
    Modify parse_input func; implement read_input func; isolate file reading process

--- gendiff/input_parser.py
+++ gendiff/input_parser.py
@@ -1,16 +1,22 @@
-from json import load as json_load
+from json import loads as json_load
 from yaml import full_load as yaml_load
 
 
-def parse_input(file_path):
+def read_input(file_path):
     with open(file_path) as file:
-        _, extension = file_path.split('.')
-        if extension == 'json':
-            return json_load(file)
-        if extension in ('yml', 'yaml'):
-            return yaml_load(file)
-        else:
-            raise TypeError(
-                f'Invalid file type .{extension}. '
-                + 'Available types are: .json, .yml, .yaml.'
-            )
+        input_ = file.read()
+        return input_
+
+
+def parse_input(file_path):
+    input_ = read_input(file_path)
+    _, extension = file_path.split('.')
+    if extension == 'json':
+        return json_load(input_)
+    if extension in ('yml', 'yaml'):
+        return yaml_load(input_)
+    else:
+        raise TypeError(
+            f'Invalid file type .{extension}. '
+            + 'Available types are: .json, .yml, .yaml.'
+        )
