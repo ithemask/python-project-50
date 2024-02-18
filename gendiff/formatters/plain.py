4e1a99d1ed0bea9b99940827722cb888868a9e31
ithemask <ai77xxxx@gmail.com>
2024-02-18 15:21:18 +0300

    Update plain.py module
    Modify plain formatter according to new diff view

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -10,13 +10,14 @@ def format_(value):
     return str(value)
 
 
-def get_plain_view(path, action, old_value, new_value):
+def get_plain_view(path, action, value):
     beginning = f"Property '{path}' was"
     if action == 'ADDED':
-        return f'{beginning} added with value: {format_(new_value)}'
+        return f'{beginning} added with value: {format_(value)}'
     if action == 'REMOVED':
         return f'{beginning} removed'
     if action == 'CHANGED':
+        old_value, new_value = value
         return (
             f'{beginning} updated. '
             f'From {format_(old_value)} to {format_(new_value)}'
@@ -28,20 +29,18 @@ def plainify(diff):
     def walk(diff, path, output):
         for entry in diff:
             key = entry['key']
-            nested = entry.get('nested')
-            if nested:
-                walk(nested, path + [key], output)
+            action = entry['action']
+            value = entry['value']
+
+            if action == 'UNCHANGED':
+                continue
+            if action == 'NESTED':
+                walk(value, path + [key], output)
             else:
-                action = entry['action']
-                old_value = entry['old value']
-                new_value = entry['new value']
-                if action == 'UNCHANGED':
-                    continue
                 output.append(get_plain_view(
                     '.'.join(path + [key]),
                     action,
-                    old_value,
-                    new_value,
+                    value,
                 ))
         return output
 
