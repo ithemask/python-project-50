fd08038838fb7c1cbdc8886ac0e73d7df4ca004f
ithemask <ai77xxxx@gmail.com>
2024-02-18 15:19:31 +0300

    Update stylish.py module
    Modify stylish formatter according to new diff view

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -30,18 +30,21 @@ def get_stylish_view(key, value, special_char, depth, indent):
     return f'{indent}{special_char}{key}: {formatted}'
 
 
-def form_string(key, action, old_value, new_value, depth, indent):
-    if action == 'ADDED':
-        return get_stylish_view(key, new_value, '+ ', depth, indent)
-    if action == 'REMOVED':
-        return get_stylish_view(key, old_value, '- ', depth, indent)
+def form_string(key, action, value, depth, indent):
     if action == 'CHANGED':
+        old_value, new_value = value
         return '\n'.join((
             get_stylish_view(key, old_value, '- ', depth, indent),
             get_stylish_view(key, new_value, '+ ', depth, indent),
         ))
-    if action == 'UNCHANGED':
-        return get_stylish_view(key, old_value, '  ', depth, indent)
+    match action:
+        case 'ADDED':
+            special_char = '+ '
+        case 'REMOVED':
+            special_char = '- '
+        case 'UNCHANGED':
+            special_char = '  '
+    return get_stylish_view(key, value, special_char, depth, indent)
 
 
 def stylishify(diff):
@@ -50,24 +53,22 @@ def stylishify(diff):
         indent = (INDENT_CHARS * depth)[0:OFFSET]
         for entry in diff:
             key = entry['key']
-            nested = entry.get('nested')
-            special_char = '  '
-            if nested:
-                output.append(f'{indent}{special_char}{key}: {{')
-                walk(nested, output, depth + 1)
+            action = entry['action']
+            value = entry['value']
+
+            if action == 'NESTED':
+                output.append(f'{indent}  {key}: {{')
+                walk(value, output, depth + 1)
                 output.append(f'{INDENT_CHARS * depth}}}')
             else:
-                action = entry['action']
-                old_value = entry['old value']
-                new_value = entry['new value']
                 output.append(form_string(
                     key,
                     action,
-                    old_value,
-                    new_value,
+                    value,
                     depth,
                     indent,
                 ))
+
         return output
 
     lines = '\n'.join(walk(diff, [], 1))
